1780e517f8efb7e2430a5bf8c179253ef47590b3
Santiago Iraola <iraola.santi@gmail.com>
2018-01-02 11:52:17 -0800

    added schema for mongodb

--- database/index.js
+++ database/index.js
@@ -3,14 +3,58 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  name: String,
+  description: String,
+  html_url: String,
+  updated_at: Date,
+  forks: Number,
+  watchers: Number,
+  owner: {
+    login: String,
+    avatar_url: String
+  }
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repoInstance) => {
+  //SHOULD CHECK FOR ID IN THE DB TO PREVENT DUPLCIATES??
+  if(Array.isArray(repoInstance)) {
+    repoInstance.forEach((repo) => {
+      repo.save((err, repo) => {
+        if(err) return console.error(err);
+
+        console.log('List of Repos have been saved:', repo);
+      });
+    });
+  } else {
+    repoInstance.save((err, repo) => {
+      if(err) return console.error(err);
+
+      console.log('Repo has been saved:', repo);
+    });
+  }
 }
+// TESTING MONGO CONNECTION
+// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+// mongoose.connection.once('open', () => {
+//   let aRepo = new Repo({
+//     "id": 18221276,
+//     "name": "git-consortium",
+//     "description": "This repo is for demonstration purposes only.",
+//     "html_url": "https://github.com/octocat/git-consortium",
+//     "updated_at": "2016-12-06T13:06:37Z",
+//     "forks": 24,
+//     "watchers": 7,
+//     "owner": {
+//       "login": "octocat",
+//       "avatar_url": "https://avatars0.githubusercontent.com/u/583231?v=3"
+//     }
+//   });
+
+//   save(aRepo);
+// });
+
 
 module.exports.save = save;
